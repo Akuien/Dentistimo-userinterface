39e12c2e0276d55eb73c6844a2c10555a94d6a27
Marwa Selwaye <75786866+Marwasel@users.noreply.github.com>
2022-11-15 21:06:10 +0100

    user interface client template

---
+++ client/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      message: 'none'
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+.btn_message {
+  margin-bottom: 1em;
+}
+</style>
