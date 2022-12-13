de313a8d070ae7e99502a2cbe9e96cc5bffe48df
akuien <akuien@student.chalmers.se>
2022-12-13 13:55:31 +0100

    Changes Update

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,11 +3,17 @@
     <div id="nav">
       <router-link to="/">Home</router-link>
       <router-link to="/userinformation">userInformation</router-link>
+      <router-link to="/Login">Login</router-link>
     </div>
     <!-- Render the content of the current page view -->
     <router-view/>
   </div>
 </template>
+<script>
+
+export default {
+}
+</script>
 
 <style>
 #app {
