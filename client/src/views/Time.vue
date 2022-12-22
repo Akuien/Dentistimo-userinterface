1e41a37f8ab0536c4d85368b4372531a99fab960
Cyn-Thea <fadzai@net.chalmers.se>
2022-12-23 00:34:39 +0100

    display only time slots of selected day

--- client/src/views/Time.vue
+++ client/src/views/Time.vue
@@ -2,14 +2,31 @@
   <b-container>
      <h1> Available Time Slots </h1>
      <h1>booking for dentist {{currentDentist.id }}</h1>
+     <b-row no-gutters>
+      <b-col cols="12">
+        <b-calendar
+          class="calendar"
+          block
+          selected-variant="success"
+          today-variant="info"
+          nav-button-variant="info"
+          v-model="value"
+          :date-disabled-fn="dateDisabled"
+          :min="min"
+          :max="max"
+          locale="en-US"
+          @selected="showTimeslots(value)"
+        >
+        </b-calendar>
+      </b-col>
+    </b-row>
      <b-container class="listitem"
          v-for="time in times"
         v-bind:key="time._id">
              <h1>dentist Name: {{ time.name  }}</h1>
-            <b-col>Opening Hour: {{ time.openinghour }}</b-col>
-            <b-col> Closing Hour: {{ time.closinghour }}</b-col>
-            <b-col> Day: {{ time.day }}</b-col>
-             <span class="feature">Opening hours:</span> <br />
+             <b-col> Day: {{ time.day }}</b-col>
+            <b-col>Opening Hours: {{ time.openinghour }}:00 - {{ time.closinghour }}:00</b-col>
+             <span class="feature">Time Slots:</span> <br />
         {{ time.timeSlots}}<br />
         <br />
           </b-container>
@@ -26,7 +43,24 @@
 <script>
 export default {
   data() {
+    const now = new Date()
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+    const minDate = new Date(today)
+    minDate.setMonth(today.getMonth())
+    minDate.setDate(today.getDate())
+    const maxDate = new Date(today)
+    maxDate.setMonth(today.getMonth() + 2)
+    maxDate.setDate(today.getDate())
     return {
+      value: '',
+      day: '',
+      min: minDate,
+      max: maxDate,
+      monday: 1,
+      tuesday: 2,
+      wednesday: 3,
+      thursday: 4,
+      friday: 5,
       selected: '',
       currentDentist: [],
       id: 0,
@@ -34,54 +68,69 @@ export default {
     }
   },
   mounted() {
-    this.$client.subscribe('ui/dentist/getdentistbyId')
-    this.$client.publish('dentists', 'The ui component wants this 1 ' + `${this.$route.params.id}` + ' dentist!!')
-    this.$client.publish('dentist/getdentistbyId', `${this.$route.params.id}`, 1, (error) => {
-      if (error) {
-        console.log(error)
-      }
-    })
-
-    this.$client.on('message', (topic, payload) => {
-      // console.log(topic, payload.toString())
-      if (topic === 'ui/dentist/getdentistbyId') {
-        // console.log('Dentist RECEIVED!!!!')
-        const response = JSON.parse(payload)
-        // console.log('Dentist: ', response)
-
-        this.currentDentist = response
-        this.id = this.currentDentist.id
-        // console.log(this.currentDentist)
-        // console.log(this.currentDentist.numberOfDentists)
-        console.log(this.id)
-      }
-      this.getUserAppointments()
-    })
-    console.log('2 test ' + this.id)
+    this.getDentist()
   },
   methods: {
-    getUserAppointments() {
-      this.$client.subscribe('ui/dentistTimeSlotsFound')
-      const denID = {
-        id: this.currentDentist.id,
-        requestid: Math.floor(Math.random() * 29805688)
-      }
-      console.log('checking id ' + this.id)
-      const newRewquest = JSON.stringify(denID)
-      this.$client.publish('dentist/getTimeslots', newRewquest)
-      console.log('step 1')
-
-      this.$client.on('message', (topic, payload) => {
-        console.log(topic, payload.toString())
-        console.log('step 2')
-        if (topic === 'ui/dentistTimeSlotsFound') {
-          const response = JSON.parse(payload)
-          // console.log('appointments: ', response)
-          this.times = response
+    dateDisabled(ymd, date) {
+    // Disable weekends (Sunday = `0`, Saturday = `6`)
+      const weekday = date.getDay()
+      // Return `true` if the date should be disabled
+      return weekday === 0 || weekday === 6
+    },
+    getDentist() {
+      this.$client.subscribe('ui/dentist/getdentistbyId')
+      this.$client.publish('dentists', 'The ui component wants this 1 ' + `${this.$route.params.id}` + ' dentist!!')
+      this.$client.publish('dentist/getdentistbyId', `${this.$route.params.id}`, 1, (error) => {
+        if (error) {
+          console.log(error)
         }
       })
-    }
+      this.$client.on('message', (topic, payload) => {
+        if (topic === 'ui/dentist/getdentistbyId') {
+          const response = JSON.parse(payload)
+          this.currentDentist = response
+          this.id = this.currentDentist.id
+        }
+      })
+    },
+    showTimeslots(date) {
+      this.$client.subscribe('ui/dentist/getdentistbyId')
+      this.$client.publish('dentists', 'The ui component wants this 1 ' + `${this.$route.params.id}` + ' dentist!!')
+      this.$client.publish('dentist/getdentistbyId', `${this.$route.params.id}`, 1, (error) => {
+        if (error) {
+          console.log(error)
+        }
+      })
+      this.$client.on('message', (topic, payload) => {
+        if (topic === 'ui/dentist/getdentistbyId') {
+          const response = JSON.parse(payload)
+          this.currentDentist = response
+          this.id = this.currentDentist.id
+        }
+        const weekday = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+        const dayOfWeek = new Date(this.value)
+        const theDay = dayOfWeek.getDay()
 
+        this.$client.subscribe('ui/dentistTimeSlotsFound')
+        const denID = {
+          id: this.currentDentist.id,
+          day: weekday[theDay],
+          date: this.value,
+          requestid: Math.floor(Math.random() * 29805688)
+        }
+        // console.log('checking date ' + this.value)
+        // console.log('checking day:::' + weekday[theDay])
+        const newRewquest = JSON.stringify(denID)
+        this.$client.publish('dentist/getTimeslots', newRewquest)
+
+        this.$client.on('message', (topic, payload) => {
+          if (topic === 'ui/dentistTimeSlotsFound') {
+            const response = JSON.parse(payload)
+            this.times = response
+          }
+        })
+      })
+    }
   }
 }
 </script>
